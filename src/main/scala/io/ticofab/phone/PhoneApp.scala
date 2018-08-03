7252503a6249643cb1eb675e45837f8b6c9b0cd9
Fabio Tiriticco <fabio.europe@gmail.com>
2018-08-03 13:50:29 +0200

    introduced clustering

--- src/main/scala/io/ticofab/phone/PhoneApp.scala
+++ src/main/scala/io/ticofab/phone/PhoneApp.scala
@@ -1,25 +1,45 @@
 package io.ticofab.phone
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import io.ticofab.phone.listener.Server
-import io.ticofab.phone.phone.Manager
+import io.ticofab.phonecommon.Messages.{CheckMatchingWith, PhoneActorReady, PhoneConnected, RegisterNode}
 import wvlet.log.LogFormatter.SourceCodeLogFormatter
-import wvlet.log.{LogSupport, Logger}
+import wvlet.log.{LogLevel, LogSupport, Logger}
 
 object PhoneApp extends App with LogSupport {
+  Logger.setDefaultFormatter(SourceCodeLogFormatter)
+  Logger.setDefaultLogLevel(LogLevel.DEBUG)
+  info("phone app starting")
+  val as = ActorSystem("showdown")
+  as.actorOf(Props[Supervisor], "supervisor")
+}
 
-  class Supervisor extends Actor with LogSupport {
-    info("starting")
+class Supervisor extends Actor with LogSupport {
+  info(s"starting, $self")
 
-    override def receive = Actor.emptyBehavior
+  // my state: the connected nodes
+  var nodes: Map[ActorRef, Int] = Map()
 
-    // start actors
-    val manager = context.actorOf(Props[Manager])
-    context.actorOf(Props(new Server(manager)))
+  def receive = {
+    case RegisterNode =>
+      nodes = nodes + (sender -> 0)
+
+    case pc: PhoneConnected =>
+      // forward it to node with minimum number of connected phones
+      val chosenNode = nodes.minBy(_._2)._1
+      chosenNode ! pc
+
+    case PhoneActorReady(phone, itsLocation) =>
+      debug(s"phone actor is ready for location $itsLocation: $phone")
+      val updatedLoad = nodes.getOrElse(sender, 0) + 1
+      nodes = nodes + (sender -> updatedLoad)
+
+      // TODO: perform load check and maybe trigger scaling or downscaling
+
+      nodes.foreach { case (node, _) => node ! CheckMatchingWith(phone, itsLocation) }
+
   }
 
-  Logger.setDefaultFormatter(SourceCodeLogFormatter)
-  info("phone app starting")
-  val as = ActorSystem("phoneapp")
-  as.actorOf(Props[Supervisor])
+  // start actors
+  context.actorOf(Props(new Server))
 }
--- src/main/scala/io/ticofab/phone/listener/Server.scala
+++ src/main/scala/io/ticofab/phone/listener/Server.scala
@@ -1,13 +1,14 @@
 package io.ticofab.phone.listener
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.Actor
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives.{complete, get, path, _}
 import akka.stream.ActorMaterializer
-import io.ticofab.phone.phone.Manager.PhoneConnected
+import io.ticofab.phonecommon.Location
+import io.ticofab.phonecommon.Messages.PhoneConnected
 import wvlet.log.LogSupport
 
-class Server(phoneManager: ActorRef) extends Actor with LogSupport {
+class Server extends Actor with LogSupport {
   override def receive = Actor.emptyBehavior
 
   implicit val as = context.system
@@ -18,7 +19,7 @@ class Server(phoneManager: ActorRef) extends Actor with LogSupport {
     // curl http://0.0.0.0:8080/connect?lat=1&lon=2
     parameters("lat".as[Int], "lon".as[Int]) { (lat, lon) =>
       info(s"phone connected at location ($lat, $lon)")
-      phoneManager ! PhoneConnected(lat, lon)
+      context.parent ! PhoneConnected(Location(lat, lon))
       complete("Connection request received\n")
     }
   } ~ get {
